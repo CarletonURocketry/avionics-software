afb96d5b1ca15f7fb0d3700a14948f0b701b10b1
Samuel Dewan <samdewan@me.com>
2019-02-04 19:40:12 -0500

    Add device serial number to did debug command output

--- src/debug-commands.c
+++ src/debug-commands.c
@@ -32,32 +32,42 @@ static void debug_version (uint8_t argc, char **argv,
 static void debug_did (uint8_t argc, char **argv,
                        struct console_desc_t *console)
 {
-    char str[16];
+    char str[11];
     
     str[0] = '0';
     str[1] = 'x';
     
-    console_send_str(console, "Device Identification: ");
+    console_send_str(console, "Serial Number: ");
+    utoa(*((uint32_t*)0x0080A00C), str + 2, 16);
+    console_send_str(console, str);
+    utoa(*((uint32_t*)0x0080A040), str + 2, 16);
+    console_send_str(console, str+2);
+    utoa(*((uint32_t*)0x0080A044), str + 2, 16);
+    console_send_str(console, str+2);
+    utoa(*((uint32_t*)0x0080A048), str + 2, 16);
+    console_send_str(console, str+2);
+    
+    console_send_str(console, "\n\nDevice Identification: ");
     utoa(DSU->DID.reg, str + 2, 16);
     console_send_str(console, str);
     
-    console_send_str(console, "\n\nPROCESSOR: ");
+    console_send_str(console, "\n\tPROCESSOR: ");
     utoa(DSU->DID.bit.PROCESSOR, str + 2, 16);
     console_send_str(console, str);
     
-    console_send_str(console, "\nFAMILY: ");
+    console_send_str(console, "\n\tFAMILY: ");
     utoa(DSU->DID.bit.FAMILY, str + 2, 16);
     console_send_str(console, str);
     
-    console_send_str(console, "\nSERIES: ");
+    console_send_str(console, "\n\tSERIES: ");
     utoa(DSU->DID.bit.SERIES, str + 2, 16);
     console_send_str(console, str);
     
-    console_send_str(console, "\nDIE: ");
+    console_send_str(console, "\n\tDIE: ");
     utoa(DSU->DID.bit.DIE, str + 2, 16);
     console_send_str(console, str);
     
-    console_send_str(console, "\nREVISION: ");
+    console_send_str(console, "\n\tREVISION: ");
     utoa(DSU->DID.bit.REVISION, str + 2, 16);
     console_send_str(console, str);
     str[2] = ' ';
@@ -67,8 +77,12 @@ static void debug_did (uint8_t argc, char **argv,
     str[6] = 0;
     console_send_str(console, str + 2);
     
-    console_send_str(console, "\nDEVSEL: ");
+    console_send_str(console, "\n\tDEVSEL: ");
     utoa(DSU->DID.bit.DEVSEL, str + 2, 16);
+    console_send_str(console, str);
+    
+    console_send_str(console, "\nCPUID: ");
+    utoa(SCB->CPUID, str + 2, 16);
     console_send_str(console, str);
     
     console_send_str(console, "\n");
