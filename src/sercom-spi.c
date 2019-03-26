3dead94cab0e65e6bbc52f285125fae500292cb2
Jake Bauer <jbauer@paritybit.ca>
2019-03-26 19:23:59 -0400

    Add special case for CS pin to be used with SD driver
    When the sercom_spi_start function is given 0xFF as a parameter for
    cs_pin_group, do not toggle the CS pin since the SD card needs the
    pin to be kept high for the entirety of its reset sequence.

--- src/sercom-spi.c
+++ src/sercom-spi.c
@@ -37,35 +37,35 @@ void init_sercom_spi(struct sercom_spi_desc_t *descriptor,
                      int8_t rx_dma_channel)
 {
     uint8_t instance_num = sercom_get_inst_num(sercom);
-    
+
     /* Enable the APBC clock for the SERCOM instance */
     PM->APBCMASK.reg |= sercom_get_pm_apb_mask(instance_num);
-    
+
     /* Select the core clock for the SERCOM instance */
     GCLK->CLKCTRL.reg = (GCLK_CLKCTRL_CLKEN |
                          core_clock_mask |
                          sercom_get_clk_id_mask(instance_num));
     // Wait for synchronization
     while (GCLK->STATUS.bit.SYNCBUSY);
-    
+
     /* Reset SERCOM instance */
     sercom->SPI.CTRLA.bit.SWRST = 0b1;
     // Wait for reset to complete
     while (sercom->SPI.SYNCBUSY.bit.SWRST);
-    
+
     /* Configure CTRL Reg A */
     // MISO on pad 2, MOSI on pad 0, SCK on pad 1, run in standby, master mode
     sercom->SPI.CTRLA.reg = (SERCOM_SPI_CTRLA_DIPO(0x2) |
                              SERCOM_SPI_CTRLA_DOPO(0x0) |
                              SERCOM_SPI_CTRLA_RUNSTDBY |
                              SERCOM_SPI_CTRLA_MODE_SPI_MASTER);
-    
+
     /* Configure CTRL Reg B */
     // Set 8 bit characters
     sercom->SPI.CTRLB.reg = (SERCOM_SPI_CTRLB_CHSIZE(0x0));
     // Wait for synchronization
     while (sercom->SPI.SYNCBUSY.bit.CTRLB);
-    
+
     /* Configure interrupts */
     sercom_handlers[instance_num] = (struct sercom_handler_t) {
         .handler = sercom_spi_isr,
@@ -74,7 +74,7 @@ void init_sercom_spi(struct sercom_spi_desc_t *descriptor,
     
     NVIC_SetPriority(sercom_get_irq_num(instance_num), SERCOM_IRQ_PRIORITY);
     NVIC_EnableIRQ(sercom_get_irq_num(instance_num));
-    
+
     /* Setup Descriptor */
     descriptor->sercom = sercom;
     descriptor->sercom_instnum = instance_num;
@@ -83,13 +83,13 @@ void init_sercom_spi(struct sercom_spi_desc_t *descriptor,
                            SERCOM_SPI_TRANSACTION_QUEUE_LENGTH,
                            descriptor->states,
                            sizeof(struct sercom_spi_transaction_t));
-   
-    
+
+
     // Configure DMA
     if ((tx_dma_channel >= 0) && (tx_dma_channel < DMAC_CH_NUM)) {
         descriptor->tx_dma_chan = (uint8_t)tx_dma_channel;
         descriptor->tx_use_dma = 0b1;
-        
+
         dma_callbacks[tx_dma_channel] = (struct dma_callback_t) {
             .callback = sercom_spi_dma_callback,
             .state = (void*)descriptor
@@ -98,7 +98,7 @@ void init_sercom_spi(struct sercom_spi_desc_t *descriptor,
     if ((rx_dma_channel >= 0) && (rx_dma_channel < DMAC_CH_NUM)) {
         descriptor->rx_dma_chan = (uint8_t)rx_dma_channel;
         descriptor->rx_use_dma = 0b1;
-        
+
         dma_callbacks[rx_dma_channel] = (struct dma_callback_t) {
             .callback = sercom_spi_dma_callback,
             .state = (void*)descriptor
@@ -117,10 +117,10 @@ uint8_t sercom_spi_start(struct sercom_spi_desc_t *spi_inst,
         return 1;
     }
     *trans_id = t->transaction_id;
-    
+
     struct sercom_spi_transaction_t *state =
                                 (struct sercom_spi_transaction_t*)t->state;
-    
+
     state->out_buffer = out_buffer;
     state->in_buffer = in_buffer;
     state->out_length = out_length;
@@ -129,13 +129,13 @@ uint8_t sercom_spi_start(struct sercom_spi_desc_t *spi_inst,
     state->cs_pin_group = cs_pin_group;
     state->cs_pin_mask = cs_pin_mask;
     state->rx_started = 0;
-    
+
     state->bytes_out = 0;
     state->bytes_in = 0;
-    
-    
+
+
     transaction_queue_set_valid(t);
-    
+
     sercom_spi_service(spi_inst);
     return 0;
 }
@@ -162,7 +162,7 @@ static void sercom_spi_service (struct sercom_spi_desc_t *spi_inst)
         // There is already a transaction in progress
         return;
     }
-    
+
     /* Acquire service function lock */
     if (spi_inst->service_lock) {
         // Could not acquire lock, service is already being run
@@ -170,7 +170,7 @@ static void sercom_spi_service (struct sercom_spi_desc_t *spi_inst)
     } else {
         spi_inst->service_lock = 1;
     }
-    
+
     struct transaction_t *t = transaction_queue_next(&spi_inst->queue);
     if (t == NULL) {
         // No pending transactions
@@ -180,10 +180,10 @@ static void sercom_spi_service (struct sercom_spi_desc_t *spi_inst)
         // Start the next transaction
         struct sercom_spi_transaction_t *s =
                                     (struct sercom_spi_transaction_t*)t->state;
-        
+
         /* Mark transaction as active */
         t->active = 1;
-        
+
         /* Set baudrate */
         uint8_t baud_error = sercom_calc_sync_baud(s->baudrate,
                                             spi_inst->core_frequency,
@@ -194,17 +194,24 @@ static void sercom_spi_service (struct sercom_spi_desc_t *spi_inst)
                                   spi_inst->core_frequency,
                                   &(spi_inst->sercom->SPI.BAUD.reg));
         }
-        
+
         /* Enable SERCOM instance */
         spi_inst->sercom->SPI.CTRLA.bit.ENABLE = 0b1;
-        
+
         /* Wait for SERCOM instance to be enabled */
         while (spi_inst->sercom->SPI.SYNCBUSY.bit.ENABLE);
-        
+
         /* Assert CS line */
+<<<<<<< HEAD
         PORT->Group[s->cs_pin_group].OUTCLR.reg = s->cs_pin_mask;
         
         /* Begin transmission */
+=======
+        if (s->cs_pin_group != 0xFF)
+            PORT->Group[s->cs_pin_group].OUTCLR.reg = s->cs_pin_mask;
+
+        /* Begin transmition */
+>>>>>>> 1aa5e52 (Add special case for CS pin to be used with SD driver)
         if (spi_inst->tx_use_dma && s->out_length) {
             // Use DMA to transmit out buffer
             dma_start_buffer_to_static(spi_inst->tx_dma_chan, s->out_buffer,
@@ -220,7 +227,7 @@ static void sercom_spi_service (struct sercom_spi_desc_t *spi_inst)
             spi_inst->sercom->SPI.INTENSET.bit.DRE = 0b1;
         }
     }
-    
+
     spi_inst->service_lock = 0;
 }
 
@@ -230,21 +237,33 @@ static inline void sercom_spi_end_transaction (
 {
     struct sercom_spi_transaction_t *s =
                                     (struct sercom_spi_transaction_t*)t->state;
-    
+
     t->done = 1;
     t->active = 0;
+<<<<<<< HEAD
     
     // Disable DRE and RXC interrupts
+=======
+
+    // Disable DRE and RXC interutps
+>>>>>>> 1aa5e52 (Add special case for CS pin to be used with SD driver)
     spi_inst->sercom->SPI.INTENCLR.reg = (SERCOM_SPI_INTENCLR_DRE |
                                           SERCOM_SPI_INTENCLR_RXC);
-    
+
     // Deassert the CS pin
+<<<<<<< HEAD
     PORT->Group[s->cs_pin_group].OUTSET.reg = s->cs_pin_mask;
     
     // Disable Receiver and SERCOM
+=======
+    if(s->cs_pin_group != 0xFF)
+        PORT->Group[s->cs_pin_group].OUTSET.reg = s->cs_pin_mask;
+
+    // Disable Reciever and SERCOM
+>>>>>>> 1aa5e52 (Add special case for CS pin to be used with SD driver)
     spi_inst->sercom->SPI.CTRLB.bit.RXEN = 0b0;
     spi_inst->sercom->SPI.CTRLA.bit.ENABLE = 0b0;
-    
+
     // Run the SPI service to start the next transaction if there is one
     sercom_spi_service(spi_inst);
 }
@@ -255,10 +274,10 @@ static inline void sercom_spi_start_reception (
 {
     struct sercom_spi_transaction_t *s =
                                     (struct sercom_spi_transaction_t*)t->state;
-    
+
     // Enable reception
     spi_inst->sercom->SPI.CTRLB.bit.RXEN = 0b1;
-    
+
     if (spi_inst->rx_use_dma) {
         // Start DMA transaction to receive data
         dma_start_static_to_buffer(spi_inst->rx_dma_chan, s->in_buffer,
@@ -270,9 +289,9 @@ static inline void sercom_spi_start_reception (
         // Enable receive complete interrupt
         spi_inst->sercom->SPI.INTENSET.bit.RXC = 0b1;
     }
-    
+
     s->rx_started = 1;
-    
+
     if (spi_inst->tx_use_dma) {
         // Start DMA transaction to write dummy bytes
         dma_start_static_to_static(
@@ -293,7 +312,7 @@ static void sercom_spi_isr (Sercom *sercom, uint8_t inst_num, void *state)
     struct transaction_t *t = transaction_queue_get_active(&spi_inst->queue);
     struct sercom_spi_transaction_t *s =
                                     (struct sercom_spi_transaction_t*)t->state;
-    
+
     // Data Register Empty
     if (sercom->SPI.INTENSET.bit.DRE && sercom->SPI.INTFLAG.bit.DRE) {
         if (s->bytes_out < s->out_length) {
@@ -325,7 +344,7 @@ static void sercom_spi_isr (Sercom *sercom, uint8_t inst_num, void *state)
             sercom->SPI.INTENCLR.bit.DRE = 0b0;
         }
     }
-    
+
     // Transmit Complete
     if (sercom->SPI.INTENSET.bit.TXC && sercom->SPI.INTFLAG.bit.TXC) {
         sercom->SPI.INTENCLR.bit.TXC = 0b1;
@@ -337,13 +356,13 @@ static void sercom_spi_isr (Sercom *sercom, uint8_t inst_num, void *state)
             sercom_spi_end_transaction(spi_inst, t);
         }
     }
-    
+
     // Receive Complete
     if (sercom->SPI.INTENSET.bit.RXC && sercom->SPI.INTFLAG.bit.RXC) {
         // Get the received byte
         s->in_buffer[s->bytes_in] = sercom->SPI.DATA.reg;
         s->bytes_in++;
-        
+
         if (s->bytes_in == s->in_length) {
             // Transaction done
             sercom_spi_end_transaction(spi_inst, t);
@@ -369,8 +388,13 @@ static void sercom_spi_dma_callback (uint8_t chan, void *state)
     
     struct sercom_spi_transaction_t *s =
                                     (struct sercom_spi_transaction_t*)t->state;
+<<<<<<< HEAD
     
     if (spi_inst->tx_use_dma && (chan == spi_inst->tx_dma_chan) &&
+=======
+
+    if (spi_inst->tx_use_dma && chan == spi_inst->tx_dma_chan &&
+>>>>>>> 1aa5e52 (Add special case for CS pin to be used with SD driver)
                                 !s->rx_started) {
         // TX stage is complete
         spi_inst->sercom->SPI.INTENSET.bit.TXC = 0b1;
