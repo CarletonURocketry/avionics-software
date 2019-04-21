4062994c1ba42ad23dceaa4cbf249daa1d187347
maximzakirov <maxim.zakirov1@gmail.com>
2019-04-21 18:41:32 -0400

    Implement altimeter altitude polling

--- src/altimeter.h
+++ src/altimeter.h
@@ -4,13 +4,30 @@
  * @author Maxim Zakirov
  * @date 2019-03-25
  * Last Author: Nobody
- * Last Edited On: 2019-03-25
+ * Last Edited On: 2019-04-20
  */
 
 #ifndef altimeter_h
 #define altimeter_h
 
 #include "global.h"
+#include "config.h"
+#include <math.h>
+
+//Definitions
+
+#define DEV_ADDRESS 0b11000000
+
+uint32_t altitude_data;
+
+//Function prototypes
+
+/* Start-up sequence for the altimeter */
+void init_altimeter(void);
+
+/* Get the Altitude from the pressure sensor */
+float get_altitude(void);
+
 
 
 #endif /* altimeter_h */
