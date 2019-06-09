f714c478d12812f4e2c4b3b634ce1e0ff2c421b6
Samuel Dewan <samdewan@me.com>
2019-06-08 23:25:32 -0400

    Make output buffer argument for I2C generic transaction const

--- src/sercom-i2c.h
+++ src/sercom-i2c.h
@@ -94,7 +94,7 @@ struct sercom_i2c_transaction_t {
          *  recieve stages */
         struct {
             /** The buffer from which data is sent. */
-            uint8_t *out_buffer;
+            uint8_t const* out_buffer;
             /** The buffer into which recieved data is placed. */
             uint8_t *in_buffer;
             /** The number of bytes to be sent. */
@@ -216,7 +216,7 @@ extern void init_sercom_i2c(struct sercom_i2c_desc_t *descriptor,
  */
 extern uint8_t sercom_i2c_start_generic(struct sercom_i2c_desc_t *i2c_inst,
                                         uint8_t *trans_id, uint8_t dev_address,
-                                        uint8_t *out_buffer,
+                                        uint8_t const* out_buffer,
                                         uint16_t out_length, uint8_t *in_buffer,
                                         uint16_t in_length);
 
