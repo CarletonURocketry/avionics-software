df524104292140d8d27f2dd0262389c452095122
Gabriel Ciolac <gabrielciolac@gmail.com>
2020-11-07 12:13:27 -0500

    Created empty header, and source files
    I would have added something before commiting, but I'm developing across two systems

---
+++ src/mpu-9250.c
@@ -0,0 +1,11 @@
+/**
+ * @file mpu-9250.h
+ * @desc driver for mpu-9250, an intertial measurement unit
+ * @author Gabriel Ciolac
+ * @date 2020-11-07
+ * @last-edited: 2020-11-07
+ */
+
+#include "mpu-9250.h"
+
+
---
+++ src/mpu-9250.h
@@ -0,0 +1,14 @@
+/**
+ * @file mpu-9250.h
+ * @desc driver for mpu-9250, an intertial measurement unit
+ * @author Gabriel Ciolac
+ * @date 2020-11-07
+ * @last-edited: 2020-11-07
+ */
+#ifndef mpu9250_h
+#define mpu9250_h
+
+
+
+
+#endif mpu9250_h
