fbdb6f1df4f7453c6007cfdc515d34affd7a7da5
Grant Wilson <wilson.m.grant@gmail.com>
2021-02-19 21:14:35 -0500

    reset settings function added

--- src/mcp2518fd.c
+++ src/mcp2518fd.c
@@ -10,22 +10,61 @@
 #include "mcp2518fd.h"
 #include "mcp2518fd-registers.h"
 
-
 /**
  * @param descriptor The pointer to where the descriptor will be stored
  */
 void init_mcp2518fd(struct mcp2518fd_desc *descriptor, struct sercom_spi_desc_t *spi_inst,
                     uint32_t cs_pin_mask, uint8_t cs_pin_group)
 {
-    /* Store SPI settings */ 
+    /* Store SPI settings */
     descriptor->spi_inst = spi_inst;
     descriptor->cs_pin_group = cs_pin_group;
     descriptor->cs_pin_mask = cs_pin_mask;
-    
+
     mcp2518fd_service(descriptor);
 }
 
 void mcp2518fd_service(struct mcp2518fd_desc *inst)
 {
+    if (sercom_spi_transaction_done(inst->spi_inst, inst->spi_out_buffer))
+    {
+        /* code */
+    }
 
+    // need to add the way that it checks if transactions are done. 
+}
+
+void mcp2518fd_reset(struct mcp2518fd_desc *inst)
+{
+    uint8_t buffer_index = inst->next_out_buffer;
+    *((inst->spi_out_buffer[buffer_index]).buffer) = 0x0000;
+    inst->spi_out_buffer[buffer_index].empty = 0;
+    set_next_out_buffer(inst);
+
+    sercom_spi_start(inst->spi_inst, &(inst->spi_out_buffer[buffer_index].spi_transaction_id),
+                     MCP2518FD_BAUD_RATE, inst->cs_pin_group, inst->cs_pin_mask,
+                     &(inst->spi_out_buffer[buffer_index].buffer), 16, 0, 0);
+    // is this the right way to not recieve?
+    // Is the out length in bits or in bytes? I've assumed bits.
+}
+
+void set_next_out_buffer(struct mcp2518fd_desc *inst)
+{
+    if (inst->spi_out_buffer[inst->next_out_buffer].empty)
+    {
+        return;
+    }
+    else
+    {
+        while (!(inst->spi_out_buffer[inst->next_out_buffer].empty))
+        {
+            (inst->next_out_buffer)++;
+            if (inst->next_out_buffer == inst->num_out_buffers)
+            {
+                inst->next_out_buffer = 0;
+            }
+            /** really need some error handling if all the buffers are full */
+        }
+        return;
+    }
 }
--- src/mcp2518fd.h
+++ src/mcp2518fd.h
@@ -10,6 +10,54 @@
 #include "sercom-spi.h"
 // #include "global.h"
 
+#define MCP2518FD_BAUD_RATE 100 // need to set the baud rate, figure out how to find it
+
+enum mcp2518fd_mode {
+    /** The value is the value to set register REQOP to for each mode */
+    /** The REQOP value does not alway correspond with the OPMOD register showing current mode */
+    MCP2518FD_CONFIGURATION_MODE, // = 0b100,
+    /** Config mode is required to change some settings and to switch between most modes */
+    /** enum State {Working = 1, Failed = 0, Freezed = 0}; */
+
+    /** NORMAL MODES */
+    MCP2518FD_NORMAL_FD_MODE, // = 0b000,
+    /** Mode to handle CAN FD and CAN 2.0 messages */
+    MCP2518FD_NORMAL_20_MODE, // = 0b110,
+    /** Supports only CAN 2.0 messages */
+    MCP2518FD_SLEEP_MODE, // = 0b001,
+    /** Low power mode preserving registers and RAM */
+    MCP2518FD_LOW_POWER_MODE,
+    /** Ultra low power mode, leaving only logic for wake up */ 
+
+    /** DEBUG MODES */
+    MCP2518FD_LISTEN_ONLY_MODE,
+    MCP2518FD_RESTRICTED_MODE,
+    /** Can only recieve and acknowledge valid frames */
+    MCP2518FD_INTERNAL_LOOPBACK_MODE,
+    MCP2518FD_EXTERNAL_LOOPBACK_MODE
+    /** Loopback modes allow transmission of messages from transmit FIFOs to recieve FIFOs */
+
+};
+
+struct mcp2518fd_config {
+    enum OSC;
+    /** Configures the oscillator */
+    
+    uint8_t spi_mode:1;
+    /** determines whether SPI is 0,0 mode or 1,1 */ 
+
+    uint8_t PLLEN:1;
+    /** Set to multiply 4MHz clock by 10 */
+    uint8_t SCLKDIV:1;
+    /** Set to divide the system clock by 2 */
+};
+
+struct spi_out_buffer{
+    uint8_t buffer[12];
+    uint8_t spi_transaction_id;
+    uint8_t empty:1;
+};
+
 struct mcp2518fd_desc {
     /** SPI instance used to communicate with the device */ 
     struct sercom_spi_desc_t *spi_inst;
@@ -17,4 +65,14 @@ struct mcp2518fd_desc {
     uint32_t cs_pin_mask;
     /** The GPIO pin group for SPI chip select */
     uint8_t cs_pin_group;
+
+    // uint8_t spi_transaction_id;
+
+    uint8_t next_out_buffer;
+    // could make this a point to the next buffer and not an index, but not sure
+    uint8_t num_out_buffers;
+
+    struct spi_out_buffer spi_out_buffer[8];
+    // How many out buffers do we need?
+    // Should we make the buffers as big as they need to be? Need to actually be 8 + the spi commands etc
 };
