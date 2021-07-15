9be1f438c9ff5a69cd922521c52788ed29ebfa8a
darwinkjull <74940510+darwinkjull@users.noreply.github.com>
2021-07-14 21:34:43 -0400

    Complete Honeywell Pressure Drivers
    First iteration of drivers.

---
+++ src/hpsens.c
@@ -0,0 +1,85 @@
+/**
+ * @file hpsens.c
+ * @desc Driver for Honeywell HSCMAND060PA3A3 Pressure sensor
+ * @author Darwin Jull  
+ * @date 2021-07-14
+ * Last Author:
+ * Last Edited On:
+ */
+
+
+#include "hpsens.h"
+
+#include <math.h>
+
+/** Max/Min output values for HSCMAND060PA3A3*/
+static const int16_t pmax = 60;
+static const int16_t pmin = 0;
+static const uint16_t outmax = 16383;
+static const uint16_t outmin = 0;
+
+//* Returns pressure in milliPSI */
+static uint32_t hpsens_pressure_math (uint16_t pressure_output)
+{
+    return ((((pressure_output - outmin) * (pmax - pmin) * 1000) / (outmax-outmin)) + pmin * 1000);
+}
+
+static uint32_t hpsens_temperature_math (uint16_t temperature_output)
+{
+    return (((temperature_output * 200 * 100) / 2047) - 5000);
+}
+
+void init_hpsens (struct hpsens_desc_t *inst,
+struct sercom_i2c_desc_t *i2c_inst, uint8_t address, uint32_t period)
+{
+    inst->address = address;
+    inst->period = period;
+
+    hpsens_service(inst);
+}
+
+void hpsens_service (struct hpsens_desc_t *inst) 
+{
+    /** exit service fxn if transaction is underway and not complete*/
+    if (inst->i2c_in_progress)
+    {
+        if (!sercom_i2c_transaction_done(inst->i2c_inst, inst->i2c_transaction_id))
+        {
+            return;
+        }
+        
+        else if (sercom_i2c_transaction_state(inst->i2c_inst,
+                                            inst->i2c_transaction_id) != I2C_STATE_DONE) 
+        {
+            return;
+        }
+        
+        inst->i2c_in_progress = 0;
+        uint16_t pressure_output = inst->sensbuffer[1] | (inst->sensbuffer[0]<< 8);
+        uint8_t status = (pressure_output >> 14)
+        if (status != 0) 
+        {
+            return;
+        }
+        inst->pressure = hpsens_pressure_math(pressure_output);
+        inst->pressurepas = 6895 * inst->pressure;
+        uint16_t temperature_output = inst->sensbuffer[3] | (inst->sensbuffer[2]<< 8);
+        temperature_output >>= 5;
+        inst->temperature = hpsens_temperature_math(temperature_output);
+    }
+    /**exit service fxn if less time than period has passed*/
+    else if ((millis - inst->last_reading_time) < inst->period)
+    {
+        return;
+    }
+    /**initiate transaction*/
+    else if (!sercom_i2c_start_generic(inst->i2c_inst, &inst->i2c_transaction_id, 
+                                        inst->address, NULL, 0 , inst->sensbuffer, 4))
+    {
+        inst->i2c_in_progress = 1;
+        inst->last_reading_time = millis;       
+        return;
+    }
+}
+
+    
---
+++ src/hpsens.h
@@ -0,0 +1,87 @@
+/**
+ * @file hpsens.h
+ * @desc Driver for Honeywell HSCMAND060PA3A3 Pressure sensor
+ * @author Darwin Jull  
+ * @date 2021-07-14
+ * Last Author:
+ * Last Edited On:
+ */
+
+
+#ifndef hpsens_h
+
+#define hpsens_h
+
+/** Main I2C address for HSCMAND060PA3A3, 
+ see nomenclature guide*/
+#define HPSENS_ADDR_MAIN 0b1011000
+
+/** Standard address for Honeywell devices is 40*/
+#define HPSENS_ADDR_40 0b0101000
+
+/** Other addresses*/
+#define HPSENS_ADDR_56 0b0111000
+
+#define HPSENS_ADDR_72 0b1001000
+
+#define HPSENS_ADDR_88 0b1011000
+
+#define HPSENS_ADDR_104 0b1101000
+
+#define HPSENS_ADDR_120 0b1111000
+
+#define HPSENS_ADDR_136 0b10001000
+
+#define HPSENS_ADDR_152 0b10011000
+
+
+#include "global.h"
+
+#include "sercom-i2c.h"
+
+
+struct hpsens_desc_t {
+
+    /** Serial driver instance, sensor address, and transaction ID */
+    struct sercom_i2c_desc_t *i2c_inst;
+    uint8_t address;
+    uint8_t i2c_transaction_id;
+
+    /** Recieved information from sensor */
+    int32_t pressure;
+    int32_t pressurepas;
+    int32_t temperature;
+    uint8_t sensbuffer[4];
+
+    /** Other information */
+    uint32_t last_reading_time;
+    uint32_t period;
+    bool i2c_in_progress = 0;
+};
+
+extern void init_hpsens (struct hpsens_desc_t *inst, struct sercom_i2c_desc_t *i2c_inst,
+ uint8_t address, uint32_t period);
+
+extern void hpsens_service (struct hpsens_desc_t *inst); 
+
+static inline int32_t hpsens_get_pressure (struct hpsens_desc_t *inst)
+{
+    return inst->pressure;
+};
+
+static inline int32_t hpsens_get_temperature (struct hpsens_desc_t *inst)
+{
+    return inst->temperature;
+};
+
+static inline uint32_t hpsens_get_last_reading_time (struct hpsens_desc_t *inst)
+{
+    return inst->last_reading_time;
+};
+
+static inline void hpsens_set_period (struct hpsens_desc_t *inst, uint32_t period)
+{
+    inst->period = period;
+};
+
+#endif
