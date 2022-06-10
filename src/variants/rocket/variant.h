ff4db5e2cc98c021824096e8a96f8815ac47f181
Samuel Dewan <samdewan@me.com>
2022-06-09 23:08:56 -0400

    Setup rocket variant.

--- src/variants/rocket/variant.h
+++ src/variants/rocket/variant.h
@@ -10,9 +10,16 @@
 #ifndef variant_h
 #define variant_h
 
-#include "gpio.h"
-
+#if !defined(telemetry_h) // don't want to include telemetry.h yet
+#define telemtry_h_skipped
+#define telemetry_h
+#endif
 #include "ms5611.h"
+#include "mpu9250.h"
+#ifdef telemtry_h_skipped
+#undef telemetry_h
+#undef telemtry_h_skipped
+#endif
 #include "rn2483.h"
 #include "radio-transport.h"
 #include "radio-antmgr.h"
@@ -26,6 +33,34 @@
 
 //
 //
+//  I2C
+//
+//
+
+/* Uncomment to initialize I2C interface */
+#define ENABLE_I2C0
+/* I2C Speed, defaults to I2C_MODE_STANDARD (100 KHz) if not defined */
+#define I2C0_SPEED I2C_MODE_FAST
+
+/* Uncomment to initialize I2C interface */
+#define ENABLE_I2C1
+/* I2C Speed, defaults to I2C_MODE_STANDARD (100 KHz) if not defined */
+#define I2C1_SPEED I2C_MODE_FAST
+
+//
+//
+//  SPI
+//
+//
+
+/* Uncomment to initialize SPI interface */
+#define ENABLE_SPI0
+
+/* Uncomment to initialize SPI interface */
+#define ENABLE_SPI1
+
+//
+//
 //  UARTs
 //
 //
@@ -36,7 +71,7 @@
 /* Baud rate for UART */
 #define UART0_BAUD 57600UL
 /* Define as one if UART should echo received bytes and provide line editing,
-    0 otherwise */
+   0 otherwise */
 #define UART0_ECHO 0
 
 // UART1
@@ -45,7 +80,7 @@
 /* Baud rate for UART */
 #define UART1_BAUD 57600UL
 /* Define as one if UART should echo received bytes and provide line editing,
-    0 otherwise */
+   0 otherwise */
 #define UART1_ECHO 0
 
 // UART2
@@ -54,7 +89,7 @@
 /* Baud rate for UART */
 #define UART2_BAUD 115200UL
 /* Define as one if UART should echo received bytes and provide line editing,
-    0 otherwise */
+   0 otherwise */
 #define UART2_ECHO 0
 
 // UART3
@@ -63,13 +98,17 @@
 /* Baud rate for UART */
 #define UART3_BAUD 9600UL
 /* Define as one if UART should echo received bytes and provide line editing,
-    0 otherwise */
+   0 otherwise */
+#define UART3_ECHO 0
 
 //
 //
 //  USB
 //
 //
+
+/* USB enabled if defined */
+#define ENABLE_USB
 
 /* Define to enable USB CDC port 0 */
 #define ENABLE_USB_CDC_PORT_0
@@ -94,7 +133,7 @@
 /* Debugging console enabled if defined */
 #define ENABLE_CONSOLE
 /* UART instance to be used for console, USB is used if not defined (and USB is
- enabled) */
+   enabled) */
 //#define CONSOLE_UART uart3_g
 /* USB CDC port to be used for debugging console, this is ignored if
    CONSOLE_UART is defined */
@@ -161,7 +200,7 @@ extern struct radio_instance_desc *const radios_g[];
  * Select the role that this endpoint should take on when it falls back into
  * search mode.
  */
-#define LORA_RADIO_SEARCH_ROLE  RADIO_SEARCH_ROLE_ADVERTISE
+#define LORA_RADIO_SEARCH_ROLE  RADIO_SEARCH_ROLE_LISTEN
 
 /*
  * Select the device address that this endpoint should use
@@ -181,12 +220,37 @@ extern struct radio_instance_desc *const radios_g[];
 /* Altimeter CSB setting */
 #define ALTIMETER_CSB 0
 /* Altimeter sample period in milliseconds */
-#define ALTIMETER_PERIOD MS_TO_MILIS(1000)
+#define ALTIMETER_PERIOD MS_TO_MILLIS(100)
 extern struct ms5611_desc_t altimeter_g;
 
 //
 //
-// GNSS
+//  IMU
+//
+//
+
+/* IMU enabled if defined */
+#define ENABLE_IMU
+/* I2C sddress for IMU */
+#define IMU_ADDR 0b1101000U
+/* IMU interrupt pin */
+#define IMU_INT_PIN GPIO_4
+
+#define IMU_GYRO_FSR            MPU9250_GYRO_FSR_2000DPS
+#define IMU_GYRO_BW             MPU9250_GYRO_BW_41HZ
+#define IMU_ACCEL_FSR           MPU9250_ACCEL_FSR_16G
+#define IMU_ACCEL_BW            MPU9250_ACCEL_BW_45HZ
+#define IMU_AG_SAMPLE_RATE      100
+#define IMU_MAG_SAMPLE_RATE     AK8963_ODR_100HZ
+#define IMU_USE_FIFO            1
+
+#ifdef ENABLE_IMU
+extern struct mpu9250_desc_t imu_g;
+#endif
+
+//
+//
+//  GNSS
 //
 //
 
@@ -194,5 +258,30 @@ extern struct ms5611_desc_t altimeter_g;
 #define ENABLE_GNSS
 /* UART used to communicate with GNSS */
 #define GNSS_UART uart2_g
+
+//
+//
+//  Logging
+//
+//
+
+#define ENABLE_LOGGING
+//#define LOGGING_START_PAUSED
+
+#ifdef ENABLE_LOGGING
+extern struct logging_desc_t logging_g;
+#endif
+
+//
+//
+//  Telemetry
+//
+//
+
+#define ENABLE_TELEMETRY_SERVICE
+
+#ifdef ENABLE_TELEMETRY_SERVICE
+extern struct telemetry_service_desc_t telemetry_g;
+#endif
 
 #endif /* variant_h */
